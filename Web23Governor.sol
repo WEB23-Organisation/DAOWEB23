44a473b09e3f339c4d59c7c4738daf357da7427c
rahul-web23 <96718709+rahul-web23@users.noreply.github.com>
2023-07-07 14:42:24 +0530

    Update Web23Governor.sol

--- Web23Governor.sol
+++ Web23Governor.sol
@@ -1,1 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+pragma experimental ABIEncoderV2;
 
+import "@openzeppelin/contracts/governance/Governor.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
+
+import "./HederaTokenService.sol";
+import "./IHederaTokenService.sol";
+import "./HederaResponseCodes.sol";
+
+contract Web23Governor is Governor, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction,HederaTokenService {
+    constructor(IVotes _token)
+        Governor("Web23Governor")
+        GovernorVotes(_token)
+        GovernorVotesQuorumFraction(4)
+    {}
+
+    function votingDelay() public pure override returns (uint256) {
+        return 1; // 1 block
+    }
+
+    function votingPeriod() public pure override returns (uint256) {
+        return 45818; // 1 week
+    }
+  
+    function proposalThreshold() public pure override returns (uint256) {
+        return 0;
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function quorum(uint256 blockNumber)
+        public
+        view
+        override(IGovernor, GovernorVotesQuorumFraction)
+        returns (uint256)
+    {
+        return super.quorum(blockNumber);
+    }
+
+}
